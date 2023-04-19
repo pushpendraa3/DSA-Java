1a3053fd4301e18f674c167a3471053d6b56e0bd
Pushpendra Singh Bhadoriya <pushpendrasingh.91650@gmail.com>
2023-04-19 10:43:59 +0530

    input and output in java

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.print("Type a number: ");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println("your roll number is " + number);
     }
 }
