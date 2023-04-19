ca8dc3b815da7b242ea4ed4e7b7e4f3d5fd3e700
Pushpendra Singh Bhadoriya <pushpendrasingh.91650@gmail.com>
2023-04-19 12:01:03 +0530

    type conversion and casting in java

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
+        System.out.println("नमस्ते");
         System.out.print("Type a number: ");
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
--- src/TypeCasting.java
+++ src/TypeCasting.java
@@ -1,5 +1,20 @@
 public class TypeCasting {
     public static void main(String[] args) {
+    int num = (int)(22.34);
+        System.out.println(num);
 
+//        automatic type promotion in expressions
+        int a = 257;
+        byte b = (byte)(a);
+        System.out.println(b);
+
+        byte first = 40;
+        byte second = 50;
+        byte third = 100;
+        int sum = first * second / third;
+        System.out.println(sum);
+
+        int number = 'a';
+        System.out.println(number);
     }
 }
